04eb699cd0b6b55884deceab3b7d2bf1eefbefa2
Sergei Yelnitski <sergei.yelnitski@gmail.com>
2022-06-14 21:38:33 +0300

    Library1.0

---
+++ src/main/java/com/library/springframework/models/Person.java
@@ -0,0 +1,51 @@
+package com.library.springframework.models;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+public class Person {
+    private int id;
+
+    @NotEmpty(message = "Name should not be empty")
+    @Size(min = 2, max = 30, message = "Name should be between 2 and 30 characters")
+    private String name;
+
+    @Min(value = 1890, message = "Birthday should be greater than 1890")
+    private int birthday;
+
+    public Person() {
+
+    }
+
+    public Person(int id, String name, int birthday) {
+        this.id = id;
+        this.name = name;
+        this.birthday = birthday;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(int birthday) {
+        this.birthday = birthday;
+    }
+
+   }
